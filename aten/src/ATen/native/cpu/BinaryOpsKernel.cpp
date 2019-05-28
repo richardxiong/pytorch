e4c890a6168b0dca24693f29dc034a868390e7b2
izdeby <iuriiz@fb.com>
2019-05-28 11:48:59 -0700

    Enabled add, sum, mul and neg for bool tensor
    gh-metadata: pytorch pytorch 21032 gh/izdeby/3/head

--- aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
+++ aten/src/ATen/native/cpu/BinaryOpsKernel.cpp
@@ -14,7 +14,7 @@ namespace {
 using namespace vec256;
 
 void add_kernel(TensorIterator& iter, Scalar alpha_scalar) {
-  AT_DISPATCH_ALL_TYPES(iter.dtype(), "add_cpu", [&]() {
+  AT_DISPATCH_ALL_TYPES_AND(ScalarType::Bool, iter.dtype(), "add_cpu", [&]() {
     auto alpha = alpha_scalar.to<scalar_t>();
     auto alpha_vec = Vec256<scalar_t>(alpha);
     binary_kernel_vec(iter,
@@ -30,13 +30,17 @@ void sub_kernel(TensorIterator& iter, Scalar alpha_scalar) {
 }
 
 void mul_kernel(TensorIterator& iter) {
-  AT_DISPATCH_ALL_TYPES(iter.dtype(), "mul_cpu", [&]() {
-    binary_kernel_vec(iter,
-      [=](scalar_t a, scalar_t b) -> scalar_t { return a * b; },
-      [=](Vec256<scalar_t> a, Vec256<scalar_t> b) {
-        return a * b;
-      });
-  });
+  if (iter.dtype() == ScalarType::Bool) {
+    binary_kernel(iter, [=](bool a, bool b) -> bool { return a && b; });
+  } else {
+    AT_DISPATCH_ALL_TYPES(iter.dtype(), "mul_cpu", [&]() {
+      binary_kernel_vec(iter,
+        [=](scalar_t a, scalar_t b) -> scalar_t { return a * b; },
+        [=](Vec256<scalar_t> a, Vec256<scalar_t> b) {
+          return a * b;
+        });
+    });
+  }
 }
 
 void div_kernel(TensorIterator& iter) {
